7158b4d2e51cf5adf44d9d1e8e0085052e692271
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2024-10-07 19:25:22 +0000

    strings

---
+++ python/string.py
@@ -0,0 +1,19 @@
+str = 'This Is a Fake String For My Test'
+
+upper = str.upper()
+print(upper)
+
+lower = str.lower()
+print(lower)
+
+if 'Fake' in str:
+    print('that\'s fake!')
+
+if 'true' not in str:
+    print('false')
+
+slice = str[2:5]
+print(slice)
+
+split = str.split(" ")
+print(split)
