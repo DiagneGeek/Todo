2bd26967f78c65f3117d92a6df65a32f322e1f07
BambaDev <166474062+DiagneGeek@users.noreply.github.com>
2024-10-08 14:48:30 +0000

    coditions,loop

--- python/dict.py
+++ python/dict.py
@@ -21,3 +21,18 @@ print(dict)
 
 dict["skills"].append('github')
 print(dict["skills"])
+
+
+if "name" in dict:
+    print("yes, bamba is in dict")
+
+if dict['age'] < 18:
+    print(f"you cannot getting a card because you are {dict['age']} ")
+
+if not ("react" in dict["skills"] and  "vue" in dict['skills']):
+    print('you are not ready')
+
+
+for skill in dict["skills"]:
+    if skill == "Python":
+        print("you are hired")
